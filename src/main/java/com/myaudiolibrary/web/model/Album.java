47e9e7434495f811f053ef09632626a9341d8c49
pitchounvivi <pitchounvivi@gmail.com>
2020-11-18 18:04:46 +0100

    création des model pour Artist et Album

--- src/main/java/com/myaudiolibrary/web/model/Album.java
+++ src/main/java/com/myaudiolibrary/web/model/Album.java
@@ -1,11 +1,45 @@
 package com.myaudiolibrary.web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "album")
 public class Album {
 
+    @Id
+    @JoinColumn(name = "AlbumId", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long AlbumId;
+
+    @Column
+    private String Title;
+
+    @ManyToOne
+    @JoinColumn(name = "ArtistId", nullable = false)
+    private Artist artist;
 
 
-    //nom des colonnes
-    //AlbumId
-    //Title
-    //ArtistId
+    public Long getAlbumId() {
+        return AlbumId;
+    }
+
+    public void setAlbumId(Long albumId) {
+        AlbumId = albumId;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
 }
--- src/main/java/com/myaudiolibrary/web/model/Artist.java
+++ src/main/java/com/myaudiolibrary/web/model/Artist.java
@@ -1,16 +1,40 @@
 package com.myaudiolibrary.web.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "artist")
 public class Artist {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "ArtistId", nullable = false)
+    private Long ArtistId;
+
+    @Column
+    private String Name;
+
+    @OneToMany(mappedBy = "artist")
+    @JsonIgnoreProperties("artist")
+    private Set<Album> albums;
 
 
+    public Long getArtistId() {
+        return ArtistId;
+    }
 
-    //nom des colonnes
-    //ArtistId
-    //Name
+    public void setArtistId(Long artistId) {
+        ArtistId = artistId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
