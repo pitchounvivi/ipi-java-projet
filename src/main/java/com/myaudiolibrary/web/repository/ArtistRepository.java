68c7558096ee55af854b09d38bfc01ef12aeb9dc
pitchounvivi <pitchounvivi@gmail.com>
2020-11-18 18:16:50 +0100

    création ArtistRepository

--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -1,2 +1,9 @@
-package com.myaudiolibrary.web.repository;public interface ArtistRepository {
+package com.myaudiolibrary.web.repository;
+
+import com.myaudiolibrary.web.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends CrudRepository <Artist, Long> {
 }
