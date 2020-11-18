b5ce4cc9494519656d59422489923ba2d593145e
pitchounvivi <pitchounvivi@gmail.com>
2020-11-18 18:16:13 +0100

    création AlbumRepository

--- src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
@@ -1,2 +1,9 @@
-package com.myaudiolibrary.web.repository;public interface AlbumRepository {
+package com.myaudiolibrary.web.repository;
+
+import com.myaudiolibrary.web.model.Album;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends CrudRepository <Album, Long> {
 }
