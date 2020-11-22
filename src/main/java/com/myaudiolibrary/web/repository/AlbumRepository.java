bb3a1647e87b552e41720d6aeb0dd8fb90d752d2
pitchounvivi <pitchounvivi@gmail.com>
2020-11-22 15:38:47 +0100

    changement de l'extend pour avoir un plus généraliste => JpaRepository

--- src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
@@ -1,9 +1,14 @@
 package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlbumRepository extends CrudRepository <Album, Long> {
+public interface AlbumRepository extends JpaRepository<Album, Integer> {
+
+
+
+
 }
--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -1,9 +1,16 @@
 package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArtistRepository extends CrudRepository <Artist, Long> {
+public interface ArtistRepository extends JpaRepository<Artist, Integer> {
+
+
+
+
+
+
 }
