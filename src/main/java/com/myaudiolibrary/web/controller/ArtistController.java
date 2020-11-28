911bc64fa74e28c7b760f482fa5add3a7e555a65
pitchounvivi <pitchounvivi@gmail.com>
2020-11-29 00:01:26 +0100

    Pagination de la liste de tous les artistes

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -3,7 +3,11 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
@@ -17,6 +21,7 @@ public class ArtistController {
     @Autowired
     private ArtistRepository artistRepository;
 
+    //Recherche d'un artiste par son id
     @RequestMapping(
             value = "/{id}",
             method = RequestMethod.GET,
@@ -29,7 +34,7 @@ public class ArtistController {
         Optional<Artist> unArtist = artistRepository.findById(id);
 
         if (!unArtist.isPresent()){
-            throw new EntityNotFoundException("L'artist : " + id + "non trouvé");
+            throw new EntityNotFoundException("L'artist avec l'id : " + id + " non trouvé.");
         }
 
         return unArtist.get();
@@ -37,6 +42,24 @@ public class ArtistController {
 
 
 
+
+    //Pagination liste de tous les artistes
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            params = {"page","size","sortProperty","sortDirection"}
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Artist> listPageArtists(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "name") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") String sortDirection
+    ){
+        Page<Artist> listPageArtists = artistRepository.findAll(PageRequest.of(page, size,
+                Sort.Direction.fromString(sortDirection), sortProperty));
+        return listPageArtists;
+    }
 
 
 }
--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -4,9 +4,10 @@ import com.myaudiolibrary.web.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Integer> {
-
 
 
 }
