feb101707e2d15169354a1a302683f5b129f211d
pitchounvivi <pitchounvivi@gmail.com>
2020-11-28 15:16:31 +0100

    suppression import inutiles

--- src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/ArtistRepository.java
@@ -2,7 +2,6 @@ package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
