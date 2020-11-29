075c8fce01064a0acacbe146d74db48e98ccd4cc
pitchounvivi <pitchounvivi@gmail.com>
2020-11-29 21:19:24 +0100

    Création d'un album et gestion de l'erreur album déjà présent

--- src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
+++ src/main/java/com/myaudiolibrary/web/controller/AlbumController.java
@@ -1,9 +1,13 @@
 package com.myaudiolibrary.web.controller;
 
+import com.myaudiolibrary.web.model.Album;
 import com.myaudiolibrary.web.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityExistsException;
 
 @RestController
 @RequestMapping(value = "/albums")
@@ -11,4 +15,24 @@ public class AlbumController {
 
     @Autowired
     private AlbumRepository albumRepository;
+
+
+    //Création d'un album
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Album createAlbum(
+            @RequestBody Album album
+    ){
+        if (albumRepository.findByTitle(album.getTitle()) != null){
+            throw new EntityExistsException("L'album : " + album.getTitle() + " existe déjà.");
+        }
+
+        return albumRepository.save(album);
+    }
+
+
 }
--- src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
@@ -1,13 +1,14 @@
 package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Album;
+import com.myaudiolibrary.web.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
 
-
-
+    //Vérification de la présence du nom
+    Album findByTitle(String title);
 
 }
