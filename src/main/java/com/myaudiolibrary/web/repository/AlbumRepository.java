446ce6622e9bb33ec3745ac35b66b20c17ad53f2
pitchounvivi <pitchounvivi@gmail.com>
2020-11-30 17:44:55 +0100

    Nettoyage des imports non utilisés dans les repository

--- src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
+++ src/main/java/com/myaudiolibrary/web/repository/AlbumRepository.java
@@ -1,7 +1,6 @@
 package com.myaudiolibrary.web.repository;
 
 import com.myaudiolibrary.web.model.Album;
-import com.myaudiolibrary.web.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
