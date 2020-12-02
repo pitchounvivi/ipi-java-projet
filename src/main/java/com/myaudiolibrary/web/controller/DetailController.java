ab31549b59c0d6459bcd86e3d0d5dbb637318888
pitchounvivi <pitchounvivi@gmail.com>
2020-12-02 22:49:32 +0100

    Recherche par nom et affichage dans une liste = ok + Commentaire du bloc affichant un artist en détail dans listeArtists.html

--- src/main/java/com/myaudiolibrary/web/controller/DetailController.java
+++ src/main/java/com/myaudiolibrary/web/controller/DetailController.java
@@ -5,6 +5,7 @@ import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Optional;
 
@@ -22,7 +24,7 @@ public class DetailController {
     @Autowired
     private ArtistRepository artistRepository;
 
-    //Affichage d'un artist
+    //Affichage d'un artiste
     @RequestMapping(
             value = "/{id}",
             method = RequestMethod.GET
@@ -38,8 +40,42 @@ public class DetailController {
     }
 
 
+    //Liste des artistes recherché par leur nom
+    @RequestMapping(
+            params = "name",
+            method = RequestMethod.GET)
+    public String searchByName(
+            final ModelMap model,
+            @RequestParam(value = "name") String nameSearch,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "name") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") String sortDirection
+    ){
+        Pageable pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+        Page<Artist> artistsByName = artistRepository.findByNameContainsIgnoreCase(nameSearch, pageRequest);
 
-    //Liste des artists
+        //gérer 404
+
+        model.put("artists", artistsByName);
+
+        //Affichage du numéro des éléments présents dans la page
+        model.put("start", page * size +1);
+        model.put("end", (page) * size + artistsByName.getNumberOfElements());
+
+        //Les boutons et l'affichage de la page en cours
+        model.put("pageNumber", page);//C'est pour afficher la page en cours
+        model.put("previousPage", page-1);
+        model.put("nextPage", page+1);
+
+        return "listeArtists";
+    }
+
+
+
+
+
+    //Liste des artistes
     @RequestMapping(
             params = {"page","size","sortProperty","sortDirection"},
             method = RequestMethod.GET)
