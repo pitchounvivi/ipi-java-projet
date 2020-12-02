6e9646701fb044dc765e6fb50526fb1f626ce8b4
pitchounvivi <pitchounvivi@gmail.com>
2020-12-02 18:35:20 +0100

    Gestion navigation vers la liste des artistes et affichage de la liste = OK

--- src/main/java/com/myaudiolibrary/web/controller/DetailController.java
+++ src/main/java/com/myaudiolibrary/web/controller/DetailController.java
@@ -3,11 +3,15 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -34,6 +38,26 @@ public class DetailController {
     }
 
 
+
+    //Liste des artists
+    @RequestMapping(
+            params = {"page","size","sortProperty","sortDirection"},
+            method = RequestMethod.GET)
+    public String listDeTousLesArtists(
+            final ModelMap model,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "matricule") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") String sortDirection
+    ){
+        Page<Artist> pageArtists = artistRepository.findAll(PageRequest.of(page, size,
+                Sort.Direction.fromString(sortDirection), sortProperty));
+
+        //gérer 404
+
+        model.put("artists", pageArtists);
+        return "listeArtists";
+    }
 
 
 
